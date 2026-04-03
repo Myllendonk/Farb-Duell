119e852fee2977f82d47dc08e2eda0bfe23dcb1b
Myllendonk <piaflora111@gmail.com>
2026-04-03 05:16:40 +0200

    Update Farben.py

--- Farben.py
+++ Farben.py
@@ -117,7 +117,7 @@ data = {
     }
     for _, row in df.iterrows()
 }
-st.write(data)
+# st.write(data)
 # alte Daten (nur Punkte) automatisch umwandeln
 for color in colors:
     if color not in data:
